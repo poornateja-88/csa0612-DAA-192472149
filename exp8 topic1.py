a2e059b2fce32f45a1f9afa61a9e9ab8a8459f00
poornateja-88 <poornatejachinni@gmail.com>
2026-07-24 16:31:02 +0530

    Delete exp8 topic1.py

--- exp8 topic1.py
+++
@@ -1,9 +0,0 @@
-def hanoi(n, s, h, d):
-    if n == 1:
-        print(s, "->", d)
-    else:
-        hanoi(n-1, s, d, h)
-        print(s, "->", d)
-        hanoi(n-1, h, s, d)
-
-hanoi(3, "A", "B", "C")
